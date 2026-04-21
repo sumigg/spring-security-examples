f86fefb99c588ada60d4ed913f9d47ef6ce043ac
sumigg <with.sumitaggarwal@gmail.com>
2026-04-21 07:29:43 +0000

    add basic auth

---
+++ src/main/java/com/prj/sec/ex/demo/security/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.prj.sec.ex.demo.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails u = User
+                .builder()
+                .username("john")
+                .password("test")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(u);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return  NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception
+    {
+      http.httpBasic(Customizer.withDefaults());
+    //  http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
+      http.authorizeHttpRequests(c -> c.anyRequest().permitAll());
+
+
+      return http.build();
+
+
+    }
+
+}
